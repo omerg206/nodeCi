232d0ac09d21d29bad88ba9a7004bc35d9358b5e
omer <omerg206@gmail.com>
2019-08-25 22:25:36 +0300

    apply exec

---
+++ services/cache.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function() {
+    console.log('im about to run a query');
+    console.log(this.getQuery);
+
+    return exec.apply(this, arguments);
+}
