54a418c13121529a1bfe67a9a2d0f7bb37c0003d
omer <omerg206@gmail.com>
2019-08-25 23:07:27 +0300

    caching works (finished video 22)

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,28 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
+
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
-    console.log('im about to run a query');
-    console.log(this.getQuery);
+mongoose.Query.prototype.exec = async function () {
+    const key = JSON.stringify({ ...this.getQuery(), colletion: this.mongooseCollection.name });
 
-    return exec.apply(this, arguments);
+    const cacheValue = await client.get(key);
+
+    if (cacheValue) {
+        console.log(cacheValue);
+
+        return JSON.parse(cacheValue);
+    }
+
+    const result = await exec.apply(this, arguments);
+
+    client.set(key, JSON.stringify(result));
+
+    return result;
 }
