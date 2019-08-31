386ae11e9b08290c175e931167b11a5a439ac20b
omer <omerg206@gmail.com>
2019-08-31 17:14:29 +0300

    testing part 20

--- tests/header.test.js
+++ tests/header.test.js
@@ -9,8 +9,8 @@ beforeEach(async () => {
     await page.goto('localhost:3000');
 });
 
-afterEach(async ()=> {
-await browser.close();
+afterEach(async () => {
+    // await browser.close();
 });
 
 
@@ -20,9 +20,31 @@ test('tthe header has the correct text', async () => {
 });
 
 test('clicking login starts auth flow ', async () => {
-   await page.click('.right a', el => el.innerHTML);
-   const url = await page.url();
+    await page.click('.right a', el => el.innerHTML);
+    const url = await page.url();
 
-   expect(url).toMatch(/accounts\.google\.com/);
+    expect(url).toMatch(/accounts\.google\.com/);
 
+});
+
+test.only('when sign in show log off button', async () => {
+    const id = '5d67fa396d674f2394dce4ed';
+
+    const Buffer = require('safe-buffer').Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    }
+
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    console.log(sessionString, sig)
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
 });
