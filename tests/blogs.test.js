87f5f3e3e3249ce57d7d3ebb4b7fcb4db75442a7
omer <omerg206@gmail.com>
2019-09-07 17:28:29 +0300

    back to 3000

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -4,7 +4,7 @@ let page;
 
 beforeEach(async () => {
     page = await Page.build();
-    await page.goto('http://localhost:3001');
+    await page.goto('http://localhost:3000');
 });
 
 afterEach(async () => {
